899c8d14f61e433e79c2e6dc7e1b67022da953f5
Sebastien Siva <ssiva@ggc.edu>
2024-02-13 16:53:31 -0500

    Lecture 2_13_2024

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    NavMeshAgent myNav;
+
     //Components Connected to other gameObjects.
     Transform playerTran;
 
@@ -13,12 +16,22 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         playerTran = GameObject.Find("Player").GetComponent<Transform>();
+        myNav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 towardsPlayer = playerTran.position - transform.position;
-        transform.position += towardsPlayer.normalized * speed * Time.deltaTime;
+        float distToPlayer = towardsPlayer.magnitude;
+        if(distToPlayer < 15) {
+            //go get em!!!!
+            myNav.SetDestination(playerTran.position);
+        }
+
+        // transform.position += towardsPlayer.normalized * speed * Time.deltaTime;
+        // transform.forward = towardsPlayer;
+
+        
     }
 }
